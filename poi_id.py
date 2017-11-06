8f4e398c2a0cc663694d16ab29579820b1f80447
Shrey <shrym.95@gmail.com>
2017-11-06 21:06:09 +0530

    Project Completed. Awaiting Further Instructions!

--- poi_id.py
+++ poi_id.py
@@ -2,6 +2,7 @@
 
 ########################################################################################
 import sys
+from time import time
 import pickle
 import pprint
 from tester import dump_classifier_and_data, main as my_main
@@ -13,78 +14,88 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 import sklearn.metrics as skm
 from sklearn.naive_bayes import GaussianNB
-from sklearn import svm
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 import matplotlib.pyplot as mp
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
+t0 = time()
 
-########################################################################################
+################################################################################################################
 def main():
     with open("final_project_dataset.pkl", "r") as data_file:
         data_dict = pickle.load(data_file)
     dataExplore(data_dict)
 
     # Step 1: Selecting Features
-    features_list = ['poi', 'salary', 'bonus', 'total_payments', 'total_stock_value',
+    temp_features_list = ['poi', 'salary', 'bonus', 'total_payments', 'total_stock_value',
                      'exercised_stock_options', 'restricted_stock', 'deferred_income',
                      'long_term_incentive', 'shared_receipt_with_poi']
 
     # Step 2: Removing Outliers
-    removeOutlier(data_dict, features_list)
+    removeOutlier(data_dict, temp_features_list)
 
     # Step 3: Engineering New Features
     data_dict = createFeature(data_dict)
-    features_list = features_list + ['poi_outgoing', 'poi_incoming', 'total_wealth']
+    features_list =  ['poi', 'exercised_stock_options', 'deferred_income',
+                      'shared_receipt_with_poi','poi_outgoing', 'poi_incoming',
+                      'total_wealth', 'poi_interaction']
+
     # Getting best features
-    features_list = get_k_best(data_dict, features_list, 10)
-    # Updated feature list
-
-    print "=> Choice of 10 best features: "
+    features_list = get_best_feats(data_dict, features_list, 6)
+    print "\n=> Best features chosen : "
     pprint.pprint(features_list)
-
+    print "\n_____________________________________________"
     # Step 4: Extracting features and labels according to feature list
     my_dataset = data_dict
     labels, features = featureExtract(my_dataset, features_list)
 
     # Step 5: Choosing and running Untuned Algorithms
-    # clf = GaussianNB()
-    # clf = DecisionTreeClassifier()
-    # clf = AdaBoostClassifier()
+
+    #clf = GaussianNB()
+    #clf = DecisionTreeClassifier()
+    #clf = AdaBoostClassifier()
 
     # Step 6 : Tuning Above Classifiers and checking Performance
 
-    # clf_nb, params_nb = tune_NB()
-    # clf_dt, params_dt = tune_DT()
-    clf_adb, params_adb = tune_ADB()
-    # clf_svc, params_svc = tune_SVC()
+    #clf_nb, params_nb = tune_NB()
+    #clf_dt, params_dt = tune_DT()
 
-    # create pipeline
-    pipeline = createPipe(clf_adb)
-    # params['pca__n_components'] = range(3,8)
-    # print pipeline.named_steps['pca'].explained_variance_ratio_
+    clf_adb, params_adb = tune_ADB()                        # Final chosen algorithm
 
-    # create GridSearch
-    #clf = GridSearchCV(pipeline, params_adb)
-    pred, labels_test, tuned_clf = gridSearchCV(pipeline, params_adb, features, labels)
+    # Create pipeline
+    scale = MinMaxScaler()
+    pca = PCA()                                             # Not used
+    estimators = [('scale', scale), ('clf', clf_adb)]
+    pipe = Pipeline(estimators)
 
-    # testing scores after tuning
-    evalMetrics(pred, labels_test)
+    # Create GridSearchCV Instance
+    grid = GridSearchCV(pipe, params_adb, scoring='precision')
+    grid.fit(features, labels)
+
+    # Final classifier
+    clf = grid.best_estimator_
+
+    print '\n=> Chosen parameters :'
+    print grid.best_params_
+
 
     ### Task 6: Dump your classifier, dataset, and features_list so anyone can
     ### check your results. You do not need to change anything below, but make sure
     ### that the version of poi_id.py that you submit can be run on its own and
     ### generates the necessary .pkl files for validating your results.
-    print "--------------- Evaluating Performance! -----------------"
-    dump_classifier_and_data(tuned_clf, my_dataset, features_list)
-    print 'dumped!'
-    my_main()
 
+    # Calling functions from tester.py for local testing
+    #print "--------------- Evaluating Performance! -----------------"
+    print "\n===> DUMPING DATASET, FEATURES AND CLASSIFIER TO PKL FILE"
+    dump_classifier_and_data(clf, my_dataset, features_list)
+    #my_main()
+
+####################################################################################################
 
 def dataExplore(data_dict) :
-    print "**** Exploratory Data Analysis ****\n____________________________________\n"
+    print '\n====> Exploratory Data Analysis!'
 
     print "=> Total points in Enron Dataset: ", len(data_dict)
     all_features = { sec_key for primary_key in data_dict.values() for sec_key in primary_key }
@@ -100,12 +111,12 @@ def dataExplore(data_dict) :
     print "=> NaN values in whole of dataset: "
     pprint.pprint(nan)
 
-    print "\n**** End of Exploratory Data Analysis ****\n__________________________________________\n"
+    print "\n_____________________________________________"
 
-### Task 2: Remove outliers
+
 def removeOutlier(data_dict, feature_list) :
     # Initial Visualisations to spot outliers
-    print "\n**** Visualisations and Outlier Removal ****\n____________________________________________\n"
+    print "\n====> Visualisations and Outlier Removal \n"
     data_to_plot = featureFormat(data_dict, feature_list, sort_keys = True)
 
     salary_index = feature_list.index('salary')
@@ -141,9 +152,10 @@ def removeOutlier(data_dict, feature_list) :
     visualise(data_to_plot, salary_index, bonus_index, 'salary', 'bonus')
     visualise(data_to_plot, total_stock_index, total_payments_index, 'total stock', 'total payments')
 
-    print "\n---> Another Outlier is observed in second plot, but it is an actual POI and is relevant."
-    print "\n**** End of Outlier Removal and Visualisations ****\n__________________________________________________\n"
+    print "=> Another Outlier is observed in second plot, but it is an actual POI."
+    print "\n_______________________________________________"
 
+# Help function for plotting
 def visualise(data, feat1, feat2, x_label, y_label ) :
     for each in data :
         x = each[feat1]
@@ -151,42 +163,54 @@ def visualise(data, feat1, feat2, x_label, y_label ) :
         mp.scatter(x,y)
     mp.xlabel(x_label)
     mp.ylabel(y_label)
-   # mp.show()                  UNCOMMENT LATER BEFORE SUBMISSION
+    mp.show()
 
 
 ### Task 3: Create new feature(s)
 
 def createFeature(data_dict) :
-
-    # Feature 1 => Total Wealth = Total Stock Value + Total payments
-    # Feature 2 => POI Interaction Ratio = Total POI Interaction / Total Interaction
-    # Replacing NaN values with 0
+    print "\n====> Engineering New Features!\n"
     finance_features = ['total_stock_value', 'total_payments', 'salary', 'bonus', 'deferred_income',
                         'exercised_stock_options', 'long_term_incentive', 'restricted_stock']
     email_features = ['to_messages', 'from_messages', 'from_poi_to_this_person', 'from_this_person_to_poi',
                       'shared_receipt_with_poi']
 
+    # Replacing NaN values with 0
     for person in data_dict.values() :
         for fin in finance_features :
             person[fin] = handleNaN(person[fin], 0)
         for em in email_features :
-            person[em] = float(handleNaN(person[em], 0)) # Conversion to float to obtain proportion later
+            person[em] = float(handleNaN(person[em], 0))    # Conversion to float to obtain proportion later
+    max_shared_receipt = 0
+    for person in data_dict.values():
+        if person['shared_receipt_with_poi'] > max_shared_receipt :
+            max_shared_receipt = person['shared_receipt_with_poi']
 
     for person in data_dict.values() :
         # Feature 1
         person['total_wealth'] = person['total_stock_value'] + person['total_payments']
         # Feature 2
-
-
         if person['from_messages'] != 0 :
             person['poi_outgoing'] = person['from_this_person_to_poi'] / person['from_messages']
         else:
             person['poi_outgoing'] = 0.0
-
+        # Feature 3
         if person['to_messages'] != 0 :
             person['poi_incoming'] = person['from_poi_to_this_person'] / person['to_messages']
         else:
             person['poi_incoming'] = 0.0
+        # Feature 4
+        total_poi_interaction = float(person['from_this_person_to_poi'] + person['from_poi_to_this_person'])
+        total_interaction = float(person['from_messages'] + person['to_messages'])
+        if total_interaction != 0.0 :
+            poi_ratio = total_poi_interaction / total_interaction
+        else :
+            poi_ratio = 0.0
+
+        person['poi_interaction'] = poi_ratio + float(person['shared_receipt_with_poi'] / max_shared_receipt)
+
+    print "_____________________________________________"
+
     return data_dict
 
 # To handle all NaN values, with desired replacement
@@ -199,19 +223,13 @@ def handleNaN(val_to_check, new_val) :
 
 ### Extract features and labels from dataset for local testing
 def featureExtract(my_dataset, features_list) :
-
+    print "\n===> Feature And Labels Extraction!"
     data = featureFormat(my_dataset, features_list, sort_keys = True)
     labels, features = targetFeatureSplit(data)
 
     return labels, features
 
-### Task 4: Try a variety of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
-# Provided to give you a starting point. Try a variety of classifiers.
+# Tuning Functions for various classifiers
 def tune_NB() :
     print "------------- Using Naive Bayes -----------------\n"
 
@@ -220,105 +238,54 @@ def tune_NB() :
 
     return nb_clf, param_grid
 
-def tune_SVC() :
-    print "------------- Using SVM -----------------\n"
-
-    svc_clf = svm.SVC()
-    param_grid = {
-        # 'pca__n_components' : range(3,10),
-        'clf__C' : [1000],
-        'clf__gamma' : [0.1],
-        'clf__kernel' : ['rbf', 'poly'],
-        'clf__random_state' : [42]
-    }
-
-    return svc_clf, param_grid
-
 def tune_DT():
     print "------------- Tuning Decision Tree -----------------\n"
     clf = DecisionTreeClassifier()
     param_grid = {
         'clf__criterion': ['entropy', 'gini'],
-        'clf__splitter': ['best', 'random']
+        'clf__splitter': ['best', 'random'],
+        'clf__min_samples_split': [2, 4, 6]
     }
+
     return clf, param_grid
 
 def tune_ADB():
-    print "------------- Using AdaBoost Ensemble -----------------\n"
-
+    print "\n===> Tuning AdaBoost Ensemble"
     clf = AdaBoostClassifier()
     param_grid = {
         'clf__algorithm' : ['SAMME', 'SAMME.R'],
-        'clf__learning_rate': [1],
-        'clf__n_estimators' : [1000]
+        'clf__learning_rate': [1, 2],
+        'clf__random_state': [42],
+        'clf__n_estimators' : [50, 65, 80, 100]
     }
 
     return clf, param_grid
 
-#####################################################################################
+def get_best_feats(data_dict, features_list, k):
 
-def createPipe(clf) :
-    print "------------------ Creating Pipeline ---------------\n"
+    # Scaling before selecting features
     scaler = MinMaxScaler()
-    pca = PCA()
-
-    estimators = [('scale', scaler), ('pca', pca), ('clf', clf)]
-    pipe = Pipeline(estimators)
-
-    print "=> Pipeline Created!"
-    return pipe
-
-#######################################################################################
-
-def gridSearchCV(pipeline, parameters, features, labels) :
-    print "---------------- Tuning Algorithm using GridSearchCV -----------------\n"
-    features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
-    print parameters, type(parameters)
-    grid_search = GridSearchCV(pipeline, parameters, cv = None, scoring='f1')
-    grid_search.fit(features_train, labels_train)
-    pred = grid_search.predict(features_test)
-
-    print '=> Best score: %0.3f' % grid_search.best_score_
-    print '=> Best parameters set:'
-    best_parameters = grid_search.best_estimator_.get_params()
-    for param_name in sorted(parameters.keys()):
-        print '--> \t%s: %r' % (param_name, best_parameters[param_name])
-    tuned_clf = grid_search.best_estimator_
-    return pred, labels_test, tuned_clf
-
-#######################################################################################
-
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-
-# Example starting point. Try investigating other evaluation techniques!
-def evalMetrics(predictions, test_labels):
-    print "---------------- Final Performance -----------------\n"
-    print "=> Accuracy Score: ", skm.accuracy_score(test_labels, predictions)
-    print "=> Precision Score: ", skm.precision_score(test_labels, predictions)
-    print "=> Recall Score: ", skm.recall_score(test_labels, predictions)
-
-### Load the dictionary containing the dataset
-def get_k_best(data_dict, features_list, k):
-
     data = featureFormat(data_dict, features_list)
     labels_train, features_train = targetFeatureSplit(data)
+    scaler.fit(features_train)
+    features_train_scaled = scaler.transform(features_train)
 
-    k_best = SelectKBest(k=k)
-    k_best.fit(features_train, labels_train)
+    skb = SelectKBest(k=k)
+    skb.fit(features_train_scaled, labels_train)
 
-    unsorted_list = zip(features_list[1:], k_best.scores_)
-    print k_best.scores_
-    sorted_list = sorted(unsorted_list, key=lambda x: x[1], reverse=True)
-    k_best_features = dict(sorted_list[:k])
+    unsorted_list = zip(features_list[1:], skb.scores_)
 
+    sorted_features = sorted(unsorted_list, key=lambda x: x[1], reverse=True)
+
+    #print "Feature Scores:\n", sorted_list
+
+    k_best_features = dict(sorted_features[:k])
     return ['poi'] + k_best_features.keys()
 
 
 
 if __name__ == '__main__':
     main()
+    print "\n_____________________________________________\n"
+    print "=> Program Completed!"
+    print '=> Time taken: ', time()-t0, ' seconds!'
